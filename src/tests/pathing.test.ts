4f002d248ad4d2db560e518500bf1baca767ecaa
Damir Lončarić <damir.loncaric987@gmail.com>
2024-12-14 13:12:50 +0100

    pathing tests done

--- src/tests/pathing.test.ts
+++ src/tests/pathing.test.ts
@@ -12,28 +12,29 @@ import {
 import { Position } from "../types";
 
 const map = [
-  ["+", "-", "-", "+", " "],
-  ["|", "@", "A", "-", "+"],
-  ["|", " ", " ", "x", "|"],
-  ["+", "B", "-", "-", "+"],
-  [" ", " ", " ", " ", " "],
+  ["+", "-", "-", "-", "+", " "],
+  ["|", " ", " ", " ", "|", " "],
+  ["|", " ", "@", "A", "-", "+"],
+  ["|", " ", " ", " ", "x", "|"],
+  ["+", "-", "B", "-", "-", "+"],
+  [" ", " ", " ", " ", " ", " "],
 ];
 
 describe("getChar", () => {
   it("should return the correct character for valid coordinates", () => {
-    expect(getChar(map, { x: 1, y: 1 })).toBe("@");
-    expect(getChar(map, { x: 2, y: 3 })).toBe("x");
+    expect(getChar(map, { x: 2, y: 2 })).toBe("@");
+    expect(getChar(map, { x: 3, y: 4 })).toBe("x");
   });
 
   it('should return " " for out-of-bounds coordinates', () => {
-    expect(getChar(map, { x: 5, y: 5 })).toBe(" ");
+    expect(getChar(map, { x: 6, y: 7 })).toBe(" ");
     expect(getChar(map, { x: -1, y: 0 })).toBe(" ");
   });
 });
 
 describe("findStartPos", () => {
   it("should find the start position correctly", () => {
-    expect(findStartPos(map)).toEqual({ x: 1, y: 1 });
+    expect(findStartPos(map)).toEqual({ x: 2, y: 2 });
   });
 
   it("should throw an error if start character is not found", () => {
@@ -83,26 +84,27 @@ describe("getTurnPathDirs", () => {
 describe("checkStraightPossible", () => {
   it("should add valid straight directions to the array", () => {
     const arr: Position[] = [];
-    const currentPos: Position = { pos: { x: 1, y: 1 }, char: "@", entryDir: "left" };
+    const currentPos: Position = { pos: { x: 2, y: 2 }, char: "@", entryDir: "left" };
     checkStraightPossible(arr, map, currentPos);
     expect(arr.length).toBe(1);
-    expect(arr[0].pos).toEqual({ x: 1, y: 2 });
+    expect(arr[0].pos).toEqual({ x: 2, y: 3 });
     expect(arr[0].char).toBe("A");
   });
 
   it("should not add invalid directions", () => {
     const arr: Position[] = [];
-    const currentPos: Position = { pos: { x: 4, y: 0 }, char: "@", entryDir: "left" };
+    const currentPos: Position = { pos: { x: 5, y: 0 }, char: "@", entryDir: "left" };
     checkStraightPossible(arr, map, currentPos);
     expect(arr.length).toBe(0);
   });
 
   it('should continue straight if on "-" or "|", ignoring turns', () => {
     const arr: Position[] = [];
-    const currentPos: Position = { pos: { x: 1, y: 2 }, char: "-", entryDir: "left" };
+    const currentPos: Position = { pos: { x: 1, y: 4 }, char: "|", entryDir: "up" };
     checkStraightPossible(arr, map, currentPos);
     expect(arr.length).toBe(1);
-    expect(arr[0].pos).toEqual({ x: 1, y: 3 }); // It should continue straight to the next valid character
+    expect(arr[0].pos).toEqual({ x: 2, y: 4 }); // It should continue straight to the next valid character
+    expect(arr[0].char).toEqual("-"); // It should continue straight to the next valid character
   });
   //TODO add more examples with other chars (-| and +)
 });
@@ -110,9 +112,11 @@ describe("checkStraightPossible", () => {
 describe("checkTurnPossible", () => {
   it("should add valid turn directions to the array", () => {
     const arr: Position[] = [];
-    const currentPos: Position = { pos: { x: 1, y: 4 }, char: "+", entryDir: "left" };
+    const currentPos: Position = { pos: { x: 2, y: 5 }, char: "+", entryDir: "left" };
     checkTurnPossible(arr, map, currentPos);
     expect(arr.length).toBe(1);
+    expect(arr[0].pos).toEqual({ x: 3, y: 5 }); // It should continue straight to the next valid character
+    expect(arr[0].char).toEqual("|"); // It should continue straight to the next valid character
   });
 
   //TODO add more examples with other chars (-| and +)
@@ -121,18 +125,18 @@ describe("checkTurnPossible", () => {
 describe("getNextPosition", () => {
   const letters: string[] = [];
   const visitedLetterCoords = new Set<string>();
-  it("should handle startChar and endChar positions", () => {
-    const currentPos: Position = { pos: { x: 1, y: 1 }, char: startChar, entryDir: "left" };
+  it("should handle startChar positions", () => {
+    const currentPos: Position = { pos: { x: 2, y: 2 }, char: startChar, entryDir: "left" };
     const nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
 
-    expect(nextPos).toEqual({ pos: { x: 1, y: 2 }, char: "A", entryDir: "left" }); // The next position after start should be straight
+    expect(nextPos).toEqual({ pos: { x: 2, y: 3 }, char: "A", entryDir: "left" }); // The next position after start should be straight
   });
 
   it('should move straight on "-" or "|" without turning', () => {
-    const currentPos: Position = { pos: { x: 0, y: 3 }, char: "+", entryDir: "right" };
+    const currentPos: Position = { pos: { x: 0, y: 4 }, char: "+", entryDir: "left" };
     const nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
 
-    expect(nextPos).toEqual({ pos: { x: 1, y: 3 }, char: "-", entryDir: "up" }); // Moving straight to the next valid position
+    expect(nextPos).toEqual({ pos: { x: 1, y: 4 }, char: "|", entryDir: "up" }); // Moving straight to the next valid position
   });
 
   it('should turn on "+"', () => {
@@ -143,15 +147,15 @@ describe("getNextPosition", () => {
   });
 
   it('should try all directions on "@"', () => {
-    const currentPos: Position = { pos: { x: 1, y: 1 }, char: "@", entryDir: "right" };
+    const currentPos: Position = { pos: { x: 2, y: 2 }, char: "@", entryDir: "right" };
     const nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
 
     // Should explore all directions, likely picking the one that moves down to a valid path
-    expect(nextPos).toEqual({ pos: { x: 1, y: 3 }, char: "A" }); // Continue downwards from '@' to ' ' (empty space)
-  }); //TODO this one fails due to multiple possibilities
+    expect(nextPos).toEqual({ pos: { x: 2, y: 3 }, char: "A", entryDir: "left" }); // Continue downwards from '@' to ' ' (empty space)
+  });
 
   it("should not save letter to list more than once", () => {
-    const currentPos: Position = { pos: { x: 1, y: 2 }, char: "A", entryDir: "right" };
+    const currentPos: Position = { pos: { x: 2, y: 3 }, char: "A", entryDir: "right" };
 
     // First visit
     let nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
@@ -165,29 +169,23 @@ describe("getNextPosition", () => {
   });
 
   it('should handle movement to a valid position when continuing straight from "-" or "|"', () => {
-    const currentPos: Position = { pos: { x: 1, y: 0 }, char: "|", entryDir: "down" };
+    const currentPos: Position = { pos: { x: 1, y: 0 }, char: "|", entryDir: "up" };
     const nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
 
-    expect(nextPos).toEqual({ pos: { x: 2, y: 0 }, char: "|" }); // Continue straight down
+    expect(nextPos).toEqual({ pos: { x: 2, y: 0 }, char: "|", entryDir: "up" }); // Continue straight down
   });
 
-  it("should handle a scenario where multiple valid directions are possible", () => {
-    const currentPos: Position = { pos: { x: 1, y: 1 }, char: "A", entryDir: "right" };
-
-    // First visit and attempt to move
-    let nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
-    expect(nextPos).toEqual({ pos: { x: 2, y: 1 }, char: " " }); // Moving downwards
-
-    // Now visiting a new letter, should not visit 'A' again
-    currentPos.pos = { x: 2, y: 1 };
-    nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
-    expect(nextPos).toEqual({ pos: { x: 3, y: 1 }, char: "B" }); // Move to 'B' as next valid char
+  it("should throw an error if there are multiple valid directions possible", () => {
+    const currentPos: Position = { pos: { x: 4, y: 2 }, char: "B", entryDir: "up" };
+    expect(() => getNextPosition(currentPos, map, letters, visitedLetterCoords)).toThrow(
+      "Too many possible directions - a fork in the road!"
+    ); // No valid next position should return undefined
   });
 
   it("should throw an error if there are no possible next positions", () => {
-    const currentPos: Position = { pos: { x: 2, y: 4 }, char: " ", entryDir: "left" }; // Edge case with no valid moves
-
-    const nextPos = getNextPosition(currentPos, map, letters, visitedLetterCoords);
-    expect(nextPos).toBeUndefined(); // No valid next position should return undefined
+    const currentPos: Position = { pos: { x: 3, y: 5 }, char: "|", entryDir: "left" }; // Edge case with no valid moves
+    expect(() => getNextPosition(currentPos, map, letters, visitedLetterCoords)).toThrow(
+      "Zero possible directions - broken path!"
+    ); // No valid next position should return undefined
   });
 });
